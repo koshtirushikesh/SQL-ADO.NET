43cbd50f2a30f5eae5efc9a80d6193846afd0712
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-01-14 13:29:46 +0530

    create a employee payroll table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,6 +1,24 @@
 create database Payroll_service
 
+create table employee_payroll
+(
+EmplayeeId int primary key identity,
+EmployeeName varchar(255),
+PhoneNumber varchar(255),
+Address varchar(255),
+Department varchar(255),
+Gender varchar(255),
+BasicPay float,
+Deductions float,
+TaxablePay float,
+Tax float,
+NetPay float,
+StartDate Date,
+City varchar(255),
+Country varchar(255)
+)
 
+drop table employee_payroll
 
 SELECT name, database_id, create_date  
 FROM sys.databases;  
