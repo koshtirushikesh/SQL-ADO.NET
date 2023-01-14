12c35f189eb5da48d6840d8aa175af2a21255fd8
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-01-14 13:41:47 +0530

    create employee payroll data

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -18,6 +18,12 @@ City varchar(255),
 Country varchar(255)
 )
 
+insert into employee_payroll(EmployeeName,BasicPay) values('Jon',20000)
+insert into employee_payroll(EmployeeName,BasicPay) values('Sita',50000)
+insert into employee_payroll(EmployeeName,BasicPay) values('Rita',45000)
+insert into employee_payroll(EmployeeName,BasicPay) values('Joe',30000)
+insert into employee_payroll(EmployeeName,BasicPay) values('Gita',15000)
+
 drop table employee_payroll
 
 SELECT name, database_id, create_date  
